957574903504bc197954048b761966e14494912f
zhuifeng <zhuifeng1017@gmail.com>
2013-08-29 18:12:18 +0800

    rename

--- FirstApp/src/com/zhao/firstapp/MainActivity.java
+++ FirstApp/src/com/zhao/firstapp/MainActivity.java
@@ -1,11 +1,18 @@
-package com.zhao.firtapp;
+package com.zhao.firstapp;
+
+import com.zhao.firstapp.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "zhaominSamplesdfad";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +24,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		Log.v(TAG, "sendMessage");
 	}
 
 }
